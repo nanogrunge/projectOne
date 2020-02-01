27e1ed7788f5d321006e14eb0340af6ee513730c
nano g <gdanielgimenez@gmail.com>
2020-02-01 20:44:31 -0300

    password lenght validation added and message error for it

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 console.log('working js');
-let form = document.getElementById('loginForm');
-let log =document.getElementById('logBtn');
-let userName = document.getElementById('userName');
-let name= userName.value;
+const form = document.getElementById('loginForm');
+let errorElement =document.getElementById('error');
+const userName = document.getElementById('userName');
+const password =document.getElementById('password');
+
 form.addEventListener('submit', logu);
 function logu(e){
+   let messages =[];
+    if(password.value.length <= 6){
+        messages.push('password must be longer than 6 characteres');
+    }
+   if(messages.length > 0){
     e.preventDefault();
-    console.log('the user name is ' + userName.value);
+    userName.value="";
+    errorElement.style.display = 'block';
+    errorElement.innerHTML =messages.join(',');
+    }
 };
