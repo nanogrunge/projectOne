d0e370865a0d3e12dbb4c98933ece9799fb6c5c9
nano g <gdanielgimenez@gmail.com>
2020-02-01 14:31:18 -0300

    js functionality to get and print user name on submit added minor html chnages

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log('working js');
+let form = document.getElementById('loginForm');
+let log =document.getElementById('logBtn');
+let userName = document.getElementById('userName');
+let name= userName.value;
+form.addEventListener('submit', logu);
+function logu(e){
+    e.preventDefault();
+    console.log('the user name is ' + userName.value);
+};
